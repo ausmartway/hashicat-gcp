e1bdf5df922b6262e3631f1626f3087f8a950b25
Yulei Liu <yulei.liu@gmail.com>
2021-06-15 19:14:17 +1000

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -5,6 +5,6 @@ module "network" {
   version = "3.3.0"
 
   network_name = "gaurav-network"
-  project_id = "var.project"
+  project_id = var.project
   subnets = [   {     subnet_name   = "gaurav-subnet" ,    subnet_ip     = "10.100.10.0/24"   ,  subnet_region = var.region   } ]
 }
